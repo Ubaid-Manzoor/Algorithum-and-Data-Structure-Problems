89c4563857691d7eeeaae4a840eca2e0ba3c58da
Ubaid Manzoor <deadhacker014@gmail.com>
2018-04-09 00:19:30 +0530

    Adding new features to Minimum cut

--- Minimum_cut_Problem/Minimum_cut_problem.cpp
+++ Minimum_cut_Problem/Minimum_cut_problem.cpp
@@ -5,7 +5,11 @@ using namespace std;
 class Graph{
 private:
     vector<pair<char,char> > Edges;
+    unordered_set<char> Set_A;
+    unordered_set<char> Set_B;
+    vector<pair<char,char> > Merged_Edges;
     int E;
+    int V;
 pair<char,char> get_random_Edge(){
     srand(time(NULL));
 	int index=rand()%E;
@@ -18,9 +22,41 @@ void swapEdges(int i,int j){
     Edges.pop_back();
     E--;
 }
+void Push_Edge_in_Merged_List(pair<char,char> temp){
+    cout<<endl<<"***"<<temp.first<<"   "<<temp.second<<endl;
+    Merged_Edges.push_back(temp);
+}
+void  Push_vertex_in_Sets(pair<char,char> temp){
+if(V > 2){
+    if(Set_A.empty()){
+        Set_A.insert(temp.first);
+        Set_A.insert(temp.second);
+    }
+    else{
+        if(Set_A.find(temp.first) != Set_A.end() || Set_A.find(temp.second) != Set_A.end()){
+            Set_A.insert(temp.first);
+            Set_A.insert(temp.second);
+        }
+        else{
+            Set_B.insert(temp.first);
+            Set_B.insert(temp.second);
+        }
+    }
+}
+else{
+    if(Set_A.find(temp.first) != Set_A.end()){
+        Set_B.insert(temp.second);
+    }
+    else{
+         Set_A.insert(temp.first);
+         Set_B.insert(temp.second);
+    }
+}
+}
 public:
-Graph(int e){
-    E = e;
+Graph(int v,int e){
+    V = v;
+    E = e*2;
 }
 void push_edge(char scr, char des){
     pair<char,char> edge;
@@ -40,35 +76,75 @@ void print_edges(){
         cout<<it->second<<")"<<endl;
     }
 }
-void minimum_cut(){
-while(E > 4){
-cout<<"het";
-    pair<char,char> temp = get_random_Edge();
-    cout<<temp.first<<"  "<<temp.second<<endl;
-    for(int i = 0 ;i < E ; i++){
-        pair<char,char> Temp = Edges[i];
-        if((temp.first == Temp.first && temp.second == Temp.second) || (temp.first == Temp.second && temp.second == Temp.first)){
-            swapEdges(i,E-1);
-            i--;
-        }
+void print_Merged_Edges(){
+     vector<pair<char,char> >::iterator it;
+     cout<<"==========Merged_Edges==========="<<endl;
+     for(it = Merged_Edges.begin(); it < Merged_Edges.end() ; it++){
+        cout<<"("<<it->first<<"<-";
+        cout<<it->second<<")"<<endl;
     }
-    for(int j = 0 ; j < E ; j++){
-        if(Edges[j].first == temp.second){
-                Edges[j].first = temp.first;
-        }
-        else if(Edges[j].second == temp.second){
-                Edges[j].second = temp.first;
-        }
-    }
-    print_edges();
 }
+void print_Set_A_and_B(){
+    unordered_set<char>::iterator it;
+    int i = 0;
+    cout<<"===========Sets=============="<<endl;
+    cout<<"(";
+    for(it = Set_A.begin(); it != Set_A.end() ; it++,i++){
+        cout<<*it;
+        if(i < Set_A.size() - 1){
+            cout<<",";
+        }
+    }
+    cout<<") ->";
+        cout<<"(";
+    for(it = Set_B.begin(); it != Set_B.end() ; it++,i++){
+        cout<<*it;
+         if(i < Set_B.size() - 1){
+            cout<<",";
+        }
+    }
+    cout<<")"<<endl;
+}
+void minimum_cut(){
+while(V > 2){
+cout<<endl<<"E = "<<E<<endl;
+        pair<char,char> temp = get_random_Edge();
+        Push_Edge_in_Merged_List(temp);
+        Push_vertex_in_Sets(temp);
+        cout<<temp.first<<"  "<<temp.second<<endl;
+        //For Loop to Pop The Edge Which is Chosed At Random
+        for(int i = 0 ;i < E ; i++){
+            pair<char,char> Temp = Edges[i];
+            if((temp.first == Temp.first && temp.second == Temp.second) || (temp.first == Temp.second && temp.second == Temp.first)){
+                swapEdges(i,E-1);
+                i--;
+            }
+            cout<<endl<<"E ==== "<<E<<endl;
+        }
+        //For Loop To Change The Edges Attaches To B(Which is merged) To A(Whom which B is merged)
+        for(int j = 0 ; j < E ; j++){
+            if(Edges[j].first == temp.second){
+                    Edges[j].first = temp.first;
+            }
+            else if(Edges[j].second == temp.second){
+                    Edges[j].second = temp.first;
+            }
+            cout<<endl<<"j ===="<<j<<endl;
+        }
+        print_edges();
+        V--;
+    }
+cout<<endl<<"Minimum is equal to >> "<<E/2<<endl;
+cout<<"Edge Left  >> ("<<Edges[0].first<<"<->"<<Edges[0].second<<")"<<endl;
+
+Push_vertex_in_Sets(Edges[0]);
 }
 };
 int main(){
-    int e;
-    cout<<"Enter number of Edges in Graph"<<endl;
-    cin>>e;
-    Graph graph(2*e);
+    int v,e;
+    cout<<"Enter number of Vertex and Edges in Graph"<<endl;
+    cin>>v>>e;
+    Graph graph(v,e);
     graph.push_edge('a','b');
     graph.push_edge('a','d');
     graph.push_edge('a','c');
@@ -76,6 +152,8 @@ int main(){
     graph.push_edge('d','b');
     graph.print_edges();
     graph.minimum_cut();
+    graph.print_Merged_Edges();
+    graph.print_Set_A_and_B();
 }
 
 
